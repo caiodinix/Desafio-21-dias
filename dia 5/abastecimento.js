d7091aa52dec7407966e30eb930f3968bbdadda6
Caio Diniz <caiodinix@gmail.com>
2024-02-08 18:52:11 -0300

    aprendendo switchcase

---
+++ dia 5/abastecimento.js
@@ -0,0 +1,27 @@
+//Definindo variaveis
+let combustivel
+let quantidade
+let pneu
+
+//Coletando dados do usuario
+combustivel = prompt('Quer abastecer com alcool ou gasolina?')
+quantidade = Number(prompt('Quantos litros?'))
+pneu = prompt('Gostaria de calibrar o pneu?')
+
+// Utilizando switchCase
+switch(combustivel) {
+    case 'gasolina':
+        console.log('Carro abastecido com ' + quantidade + ' litros de gasolina' + ' valor a pagar ' + 'R$' +
+            5*quantidade)
+        break;
+    case 'alcool':
+        console.log('Carro abastecido com ' + quantidade + ' litros de álcool'+ ' valor a pagar ' + 'R$' +
+            3*quantidade)
+        break;
+    default:
+        console.log('Escolha o combustivel corretamente')
+        break;
+}
+if(pneu == 'sim') {
+    console.log('pneus calibrados com sucesso')
+}
